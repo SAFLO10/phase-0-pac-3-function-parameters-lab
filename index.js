90eae402a9aa2cf8a3b8185d31055b274f2051d1
Sebastian <seb154@live.com>
2023-09-25 15:08:52 -0400

    Created functions

---
+++ index.js
@@ -0,0 +1,20 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+  function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    console.log(introductionWithLanguage("name", "Javascript"));
+}
+  
+  function introductionWithLanguageOptional(name, language) {
+    if (language === undefined) {
+      language = "JavaScript";
+    }
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  function introductionWithLanguageOptional(name, language) {
+    if (language === undefined) {
+      language = "JavaScript";
+    }
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
